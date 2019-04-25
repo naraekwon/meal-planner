bb17bb1970098e2c9b83f6ccbe1ab7bef96a696d
NaraeKwon <nali4214@gmail.com>
2019-04-24 21:56:22 -0700

    simplified the basic printing operation using iterable

--- designs.js
+++ designs.js
@@ -15,7 +15,7 @@ mealAmount.addEventListener('submit', function(e) {
 
 
 function plan(b, l, d, s){
-	// let iterable = new Map([[breakfast.value, 'breakfast'], [lunch.value, 'lunch'], [dinner.value, 'dinner'], [snack.value, 'snack']]);
+	let iterable = new Map([[breakfast.value, 'breakfast'], [lunch.value, 'lunch'], [dinner.value, 'dinner'], [snack.value, 'snack']]);
 	
 	//remove existing table using while loop
 	while (table.firstElementChild !== null){
@@ -24,56 +24,15 @@ function plan(b, l, d, s){
 	}
 
 	//create a new weekly plan
-	// for (let entry of iterable) {
-	// 	newTableRow = document.createElement('tr');
-	// 	for (let i = 1; i <= entry[0]; i ++){
-	// 		newList = document.createElement('ul');
-	// 		newList.innerHTML = entry[1] + i;
-	// 		newTableRow.appendChild(newList);
-	// 	}
-	// 	table.appendChild(newTableRow);
-	// }
-
-
-	newTableRow = document.createElement('tr');
-	for(let i = 1; i <= b; i++){
-		newList = document.createElement('ul');
-		newList.innerHTML = "Breakfast" + i;
-		newTableRow.appendChild(newList);
-		if (i == b) {
-			table.appendChild(newTableRow);
+	for (let entry of iterable) {
+		newTableRow = document.createElement('tr');
+		for (let i = 1; i <= entry[0]; i ++){
+			newList = document.createElement('ul');
+			newList.innerHTML = entry[1] + i;
+			newTableRow.appendChild(newList);
+			if (i == entry[0]) {
+				table.appendChild(newTableRow);
+			};
 		};
-	};
-	
-
-	newTableRow = document.createElement('tr');
-	for(let i = 1; i <= l; i++){
-		newList = document.createElement('ul');
-		newList.innerHTML = "Lunch" + i;
-		newTableRow.appendChild(newList);
-		if (i == l) {
-			table.appendChild(newTableRow);
-		};
-	};
-
-
-	newTableRow = document.createElement('tr');
-	for(let i = 1; i <= d; i++){
-		newList = document.createElement('ul');
-		newList.innerHTML = "Dinner" + i;
-		newTableRow.appendChild(newList);
-		if (i == d) {
-			table.appendChild(newTableRow);
-		};
-	};
-
-	newTableRow = document.createElement('tr');
-	for(let i = 1; i <= s; i++){
-		newList = document.createElement('ul');
-		newList.innerHTML = "Snack" + i;
-		newTableRow.appendChild(newList);
-		if (i == s) {
-			table.appendChild(newTableRow);
-		};
-	};
+	}
 }
